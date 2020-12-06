e2d08ee386319e2e1687352ace0cac2d1eecd37c
Atichka <atichka@MacBook-Air-Ekaterina.local>
2020-12-06 15:51:48 +0300

    add class card-list__hide

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./style.css";
+
+const messageButton = document.querySelector('.message__button');
+const cardList = document.querySelector('.card-list');
+
+messageButton.addEventListener('click', visibleMessage);
+
+function visibleMessage () {
+  cardList.classList.toggle('card-list__hide');
+}
