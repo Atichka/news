85d755aa36990c76d791a4f9351343a72d04f65f
Atichka <atichka@MacBook-Air-Ekaterina.local>
2020-12-06 23:51:26 +0300

    add class cardList, card and sum news

--- src/index.js
+++ src/index.js
@@ -1,10 +1,66 @@
 import "./style.css";
 
+class Card {
+  constructor(title, autor, dataTime, link, status) {
+    this.cardElement = this.create(title, autor, dataTime, link, status);
+  }
+
+  create(titleValue, autorValue, dataTimeValue, linkValue, statusValue) {
+    const element = document.createRange().createContextualFragment(`<div class="card">
+    <h2 class="card__title">${titleValue}</h3>
+    <p class="card__autor">${autorValue}</p>
+    <p class="card__data-time">${dataTimeValue}</p>
+    <a href=${linkValue} class="card__link">Подробнее...</a>
+    <p class="card__status">${statusValue}</p>
+  </div>`)
+    return element;
+  }
+}
+
+class CardList {
+  constructor(container) {
+    this.container = container;
+  }
+
+  render(cards) {
+    cards.forEach(element => {
+      console.log(element);
+      this.addCard(element.title,
+        element.autor,
+        element.dataTime,
+        element.link,
+        element.status);
+    });
+  }
+
+  addCard(title, autor, dataTime, link, status) {
+    const { cardElement } = new Card(title,
+      autor,
+      dataTime,
+      link,
+      status);
+    this.container.appendChild(cardElement);
+  }
+}
+
 const messageButton = document.querySelector('.message__button');
 const cardList = document.querySelector('.card-list');
+const messageValue = document.querySelector('.message__value');
+const cardListObj = new CardList(document.querySelector('.card-list'));
+const card = new Card();
 
 messageButton.addEventListener('click', visibleMessage);
 
 function visibleMessage () {
   cardList.classList.toggle('card-list__hide');
 }
+
+window.addEventListener('load',function()
+{
+  fetch('./news.json')
+  .then(response => response.json())
+  .then((obj) => {
+    messageValue.textContent = Object.keys(obj).length;
+    return cardListObj.render(obj);
+  })
+})
